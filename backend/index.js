e408589dcd6417871bf16db9efbc393757bf02e7
RyanSamman <RyanSamman@outlook.com>
2020-08-22 04:55:25 +0300

    Move database initialization into index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
-const config = require('./config');
-const logger = require('./util/logger');
-
-const http = require('http');
-const app = require('./app');
-
-const server = http.createServer(app);
-
 process.on('unhandledRejection', e => {
 	logger.error(e);
 	process.exit(1);
 });
 
-server.listen(config.PORT, () => logger.info(`Server running on ${config.PORT}`));
+const http = require('http');
+const config = require('./config');
+const logger = require('./util/logger');
+const app = require('./app');
+const { loadDatabase } = require('./loaders/database');
+
+const main = async () => {
+	const server = http.createServer(app);
+	
+	await loadDatabase(config.DB_URI);
+
+	server.listen(config.PORT, () => logger.info(`Server running on ${config.PORT}`));
+};
+
+main();
