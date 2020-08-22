bf80d81a7a235285abbb5277a4a866dd1203f420
RyanSamman <RyanSamman@outlook.com>
2020-08-22 19:11:49 +0300

    Log DB_URI upon error in db connection

--- backend/loaders/database.js
+++ backend/loaders/database.js
@@ -4,7 +4,7 @@ const logger = require('../util/logger');
 const loadDatabase = async (DB_URI) => {
 	await mongoose.connect(`${DB_URI}&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 		.then(() => logger.info(`Database Connection Established ${mongoose.connection.host}:${mongoose.connection.port}`))
-		.catch(e => logger.error(`Error while connecting to Database: ${e}`));
+		.catch(e => logger.error(`Error while connecting to Database: ${e} (Connection URI: ${DB_URI})`));
 };
 
 module.exports = { loadDatabase };
