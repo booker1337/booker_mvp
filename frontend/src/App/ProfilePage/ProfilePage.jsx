413f86f6b9532d12f7b4bd7fcc73d4862ceb047c
Anton Yershov <anthony.yershov@gmail.com>
2020-08-23 17:40:23 -0700

    started onboard flow

--- frontend/src/App/ProfilePage/ProfilePage.jsx
+++ frontend/src/App/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import userProfileService from '../../utils/userProfileService';
 
-const ProfilePage = ({ user }) => {
+import { Redirect } from 'react-router-dom';
+
+const ProfilePage = ({ user, history }) => {
+  const [onboardDone, setOnboardDone] = useState(undefined);
+
+  useEffect(() => {
+    userProfileService.getUserProfile(user);
+    setOnboardDone(false) // replace false with condition for: has the user finished onboarding yet?
+  }, [user, history]);
+
+
+  if (onboardDone === false) return <Redirect to="/start" />
+  
+  else if (onboardDone === true)
+    return (
+      <div>Profile Page{JSON.stringify(user)}</div>
+    );
+  
   return (
-    <div>{JSON.stringify(user)}</div>
+    <div>checking onboard status...</div>
   );
 };
 
