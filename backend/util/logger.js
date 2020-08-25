77919f3641115f4de760366bb027e5737641ae83
RyanSamman <RyanSamman@outlook.com>
2020-08-26 02:43:21 +0300

    Less buggy logger
    Will log everything by default

--- backend/util/logger.js
+++ backend/util/logger.js
@@ -1,14 +1,16 @@
 const chalk = require('chalk');
 
+const LOGGER_LEVEL = process.env.LOGGER_LEVEL;
+
 module.exports = {
 	silly: (...params) => {
 		if (process.env.NODE_ENV === 'test') return;
-		if (!process.env.LOGGER_LEVEL.match(/^silly$/i)) return;
+		if (LOGGER_LEVEL && !LOGGER_LEVEL.match(/(|silly)/i)) return;
 		console.log(...params);
 	},
 	info: (...params) => {
 		if (process.env.NODE_ENV === 'test') return;
-		if (!process.env.LOGGER_LEVEL.match(/^(|info|silly)$/i)) return;
+		if (LOGGER_LEVEL && !LOGGER_LEVEL.match(/(|info|silly)/i)) return;
 		console.log(...params);
 	},
 	error: (...params) => {
