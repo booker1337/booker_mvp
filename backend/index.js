5d7374a2af191aff3a957d128fa3f14cf2d83d2d
Anthony Yershov <33523460+ayershov777@users.noreply.github.com>
2020-09-01 21:13:32 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const path = require('path')
-const config = require('./config');
-const logger = require('./util/logger');
-const express = require('express');
-<<<<<<< HEAD
-<<<<<<< HEAD
-const { requestLogger } = require('./util/middleware');
-=======
->>>>>>> 86798042af81d12e05d4ec3ec1952597538df15f
-=======
->>>>>>> 54bf0958472e324395168c614223f303908fd760
-const app = express();
-require('express-async-errors');
-const { requestLogger, errorHandler } = require('./util/middleware');
-
-const cors = require('cors');
-
-require('./config/database');
-
-=======
->>>>>>> 742e9d95734bf5ced943ab46ce94348b0e00ade3
 process.on('unhandledRejection', e => {
 	logger.error(e);
 	process.exit(1);
